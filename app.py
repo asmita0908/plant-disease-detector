3b3e0100c51263d21f84f94a91ff074dc8e6759a
Asmita <asmita.baghsavar.csiot22@ggits.net>
2026-02-21 18:38:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.models import load_model
 from PIL import Image
 import json
 
@@ -10,7 +9,7 @@ st.set_page_config(page_title="Plant Disease Detector", layout="centered")
 st.title("🌿 Plant Disease Detection System")
 st.write("Upload a leaf image and the AI model will detect the disease.")
 
-# ---- LOAD MODEL ----
+# Load model
 @st.cache_resource
 def load_my_model():
     model = tf.keras.models.load_model("model.h5", compile=False)
@@ -18,13 +17,13 @@ def load_my_model():
 
 model = load_my_model()
 
-# ---- LOAD LABELS ----
+# Load class labels
 with open("class_indices.json") as f:
     class_indices = json.load(f)
 
 labels = {v: k for k, v in class_indices.items()}
 
-# ---- UPLOAD IMAGE ----
+# Upload image
 uploaded_file = st.file_uploader("Upload Leaf Image", type=["jpg","jpeg","png"])
 
 if uploaded_file is not None:
@@ -32,8 +31,9 @@ if uploaded_file is not None:
     st.image(img, caption="Uploaded Image", use_column_width=True)
 
     img = img.resize((224,224))
-    img_array = np.array(img)/255.0
+    img_array = np.array(img)
     img_array = np.expand_dims(img_array, axis=0)
+    img_array = img_array / 255.0
 
     prediction = model.predict(img_array)
     index = np.argmax(prediction)
