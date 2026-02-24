dc1f511e1d60281fedb5570c7f642ef2683a2a92
Asmita <asmita.baghsavar.csiot22@ggits.net>
2026-02-24 11:55:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ MODEL_ID = "1Lu752xt99Nbi5Lsh09hIg9nSJ0m-wREo"
 MODEL_PATH = "plant_disease_model.h5"
 
 def download_model():
-if not os.path.exists(MODEL_PATH):
+  if not os.path.exists(MODEL_PATH):
 with st.spinner("Downloading AI model... first time only (2-3 minutes)"):
 url = f"https://drive.google.com/uc?id={MODEL_ID}"
 gdown.download(url, MODEL_PATH, quiet=False)
@@ -25,7 +25,7 @@ download_model()
 
 @st.cache_resource
 def load_model():
-model = tf.keras.models.load_model(MODEL_PATH, compile=False)
+  model = tf.keras.models.load_model(MODEL_PATH, compile=False)
 return model
 
 model = load_model()
@@ -51,7 +51,7 @@ image = np.expand_dims(image, axis=0)
 return image
 
 if uploaded_file is not None:
-image = Image.open(uploaded_file).convert("RGB")
+  image = Image.open(uploaded_file).convert("RGB")
 st.image(image, caption="Uploaded Image", use_column_width=True)
 
 
